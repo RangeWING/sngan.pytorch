27c68c5e207df32acf6a7d366eb1c587885dc50d
Younggeol Cho <rangewing@kaist.ac.kr>
2022-10-28 00:00:37 +0900

    parameter update

--- exps/gen_celeba.sh
+++ exps/gen_celeba.sh
@@ -5,8 +5,8 @@ export CUDA_VISIBLE_DEVICES=0
 rm -rf out/*
 
 python generate.py \
---random_seed 20150717 \
---load_path ./logs/sngan_64_2022_10_25_15_32_14/Model/checkpoint_79.pth \
+--random_seed 1234 \
+--load_path ./logs/sngan_64_2022_10_28_01_23_45/Model/checkpoint_10.pth \
 --out_path ./out \
 --latent_dim 100 \
 --img_size 64 \
